3bd11a6daef1ace99a5535061fd3a6fbb89bc23c
Woohyun Kim <r45r54r45@naver.com>
2016-08-26 22:39:07 +0900

    woohyun update

--- app/pages/teach-modal/teach-modal.ts
+++ app/pages/teach-modal/teach-modal.ts
@@ -2,30 +2,28 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {Slides} from 'ionic-angular';
 import {ViewChild} from '@angular/core';
-import { ModalController } from 'ionic-angular';
-import { TeachModalPage} from '../teach-modal/teach-modal';
 /*
-  Generated class for the TeachPage page.
+  Generated class for the TeachModalPage page.
 
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
 */
 @Component({
-  templateUrl: 'build/pages/teach/teach.html',
+  templateUrl: 'build/pages/teach-modal/teach-modal.html',
 })
-export class TeachPage {
+export class TeachModalPage {
   mySlideOptions = {
     initialSlide: 0,
     loop: false
   };
   @ViewChild('mySlider') slider:Slides;
-  constructor(private navCtrl: NavController, private Modal : ModalController) {
+  constructor(private navCtrl: NavController) {
+
   }
   slideMove(moveTo) {
     this.slider.slideTo(moveTo, 500);
   }
-  addClass(){
-    let modal=this.Modal.create(TeachModalPage);
-    modal.present();
+  backButton(){
+    this.navCtrl.pop();
   }
 }
